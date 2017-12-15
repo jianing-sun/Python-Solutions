cc0496895524401f7282a526a9d595007d0d454b
Jianing Sun <jianing.sun@mail.mcgill.ca>
2017-12-15 17:58:43 -0500

    Update Sorting.py

--- Classic Algorithms/Sorting.py
+++ Classic Algorithms/Sorting.py
@@ -2,9 +2,11 @@
     Implement two types of sorting algorithms:
     Merge sort and bubble sort
 '''
+
+
 # str_in = input('Please input the numbers you want to be order, separated with a space: ')
 # num = [int(n) for n in str_in.split()]
-test = [10, 5, 2, 3, 7, 4, 8, 9]
+
 
 
 def merge(left, right):
@@ -22,7 +24,7 @@ def merge(left, right):
     return output
 
 
-def merge_func(num):   # recursion
+def merge_func(num):  # recursion
     if len(num) == 1:
         return num
     middle = int(len(num) / 2)
@@ -31,7 +33,24 @@ def merge_func(num):   # recursion
     return merge(left_num, right_num)
 
 
+def bubble_sort(num):
+    # output = []
+    i, j = 0, 0
+    while i < len(num):
+        i += 1
+        j = 0
+        while j < (len(num) - i):
+            if num[j] > num[j + 1]:
+                temp = num[j]
+                num[j] = num[j + 1]
+                num[j + 1] = temp
+            j += 1
+    return num
+
+
 if __name__ == '__main__':
+    test = [10, 5, 2, 3, 7, 4, 8, 9]
     str_in = input('Please input the numbers you want to be order, separated with a space: ')
     num = [int(n) for n in str_in.split()]
+    print(bubble_sort(num))
     print(merge_func(num))
